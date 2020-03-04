209b722844741ed656634c138f57d876d872dfc5
sroblesher1 <sroblesher1@miners.utep.edu>
2020-03-04 15:49:24 -0600

    Finished Method and 4 test cases

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,30 +1,29 @@
 public class SortOfSort {
-    public static int[] sortOfSort(int[] arr) {
-        return sortOfSort(arr, 0, arr.length-1, 0);
+    public static void sortOfSort(int[] arr) {
+        sortOfSort(arr, 0, arr.length-1, 0);
     }
 
-    public static int[] sortOfSort(int[] arr, int l, int r, int x) {
+    public static void sortOfSort(int[] arr, int l, int r, int x) {
         int a = findLargest(arr, l, r);
 
-        if (r >= l) {
+        if (r > l) {
             if (x == 0) {
                 swap(arr, r, a);
-                return sortOfSort(arr, l, r-1,1);
+                sortOfSort(arr, l, r-1,1);
             }
             else if (x == 1) {
                 swap(arr, r,a);
-                return sortOfSort(arr, l, r-1, 2);
+                sortOfSort(arr, l, r-1, 2);
             }
             else if (x == 2) {
                 swap(arr,l,a);
-                return sortOfSort(arr,l+1,r,3);
+                sortOfSort(arr,l+1,r,3);
             }
             else if (x == 3) {
                 swap(arr,l,a);
-                return sortOfSort(arr, l+1,r,0);
+                sortOfSort(arr, l+1,r,0);
             }
         }
-        return arr;
     }
 
     public static int findLargest(int[] arr, int l, int r) {
--- Java/src/SortOfSortTester.java
+++ Java/src/SortOfSortTester.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SortOfSortTester {
 
     @org.junit.jupiter.api.Test
-    void sortOfSort() {
+    void sortOfSort1() {
         int arr[] = {2,7,1,3,0,9,6,5};
 
         SortOfSort a = new SortOfSort();
@@ -11,6 +11,45 @@ public class SortOfSortTester {
         a.sortOfSort(arr);
 
         int arr2[] = {6,5,1,0,2,3,7,9};
+
+        assertArrayEquals(arr, arr2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortOfSort2() {
+        int arr[] = {2};
+
+        SortOfSort a = new SortOfSort();
+
+        a.sortOfSort(arr);
+
+        int arr2[] = {2};
+
+        assertArrayEquals(arr, arr2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortOfSort3() {
+        int arr[] = {};
+
+        SortOfSort a = new SortOfSort();
+
+        a.sortOfSort(arr);
+
+        int arr2[] = {};
+
+        assertArrayEquals(arr, arr2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortOfSort4() {
+        int arr[] = {};
+
+        SortOfSort a = new SortOfSort();
+
+        a.sortOfSort(arr);
+
+        int arr2[] = {};
 
         assertArrayEquals(arr, arr2);
     }
